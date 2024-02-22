e3baf9a6b8b9d274f15724f61c80c70f461a9ca8
hermes <lorenzharfst@gmail.com>
2024-02-22 23:13:25 +0100

    add error handling for MongoDB connection errors

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
     res.send('GET succesful!');
 })
 
-mongoose.connect('mongodb://192.168.1.46:27017/').then(() => console.log('Connected to MongoDB.'));
+mongoose.connect('mongodb://192.168.1.46:27017/')
+    .then(() => console.log('Connected to MongoDB.'))
+    .catch((reason) => console.log(`MongoDB connection refused: ${reason}.`));
 
 app.listen(3030, () => {console.log("Listening on port 3030.")});
