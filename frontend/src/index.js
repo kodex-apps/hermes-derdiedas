0e0b3154465049c5d8e7e9f6eaf5be399200d01f
HERMES <lorenzharfst@gmail.com>
2024-02-22 18:18:38 +0000

    fix createBrowserRouter missing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 
-const router = ([
+const router = createBrowserRouter([
   {
     path: '/',
     element: <App />
