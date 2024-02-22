87f41a0b99ad9d4976f2637a482ea8cb40f8c17e
hermes <lorenzharfst@gmail.com>
2024-02-22 23:02:17 +0100

    create connection string with MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('GET succesful!');
 })
+
+mongoose.connect('mongodb://192.168.1.46:27017/').then(() => console.log('Connected to MongoDB.'));
 
 app.listen(3030, () => {console.log("Listening on port 3030.")});
