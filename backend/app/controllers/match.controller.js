a5e29c18a38f29bd5d5a1a7548ec8074e0809d68
hermes <lorenzharfst@gmail.com>
2024-02-26 21:57:55 +0100

    change Match schema to include owner

--- backend/app/controllers/match.controller.js
+++ backend/app/controllers/match.controller.js
@@ -8,10 +8,10 @@ exports.findOne = (req, res) => {
 
     Match.find({shortId: id})
         .then(data => {
-            
+           if (!data) res.status(404).send({message: `Couldn't find match ${id}`});
+           else res.send(data);
         })
         .catch(err => {
-
-        })
-
+            res.status(500).send({message: `${err}`})
+        });
 }
--- backend/app/models/match.model.js
+++ backend/app/models/match.model.js
@@ -2,6 +2,7 @@ module.exports = mongoose => {
     const Match = mongoose.model(
         "match",
         mongoose.Schema({shortID: Number,
+        owner: String,
         wordList: Array,
         playerList: Array},
         {timestamps: true})
