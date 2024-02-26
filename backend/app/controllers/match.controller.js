38ab6038dfce89b597f2e99db51dd46d4bf1767e
hermes <lorenzharfst@gmail.com>
2024-02-26 22:14:57 +0100

    create temporal find/create functions for Matches

--- backend/app/controllers/match.controller.js
+++ backend/app/controllers/match.controller.js
@@ -15,3 +15,20 @@ exports.findOne = (req, res) => {
             res.status(500).send({message: `${err}`})
         });
 }
+
+exports.create = (req, res) => {
+    const matchOwner = req.params.ownerName;
+    // Temp shortId for testing purposes. TODO: Create the actual generation of a short and unique id for each match, made of numbers?
+    const minCeilied = Math.ceil(1);
+    const maxFloored = Math.floor(1000);
+    const randomShortId = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+    // Temp wordList for testing purposes.
+    const wordList = [["Die", "Frau", "Der", "Mann", "Das", "Kind", "Das", "Auto", "Der", "Teufel", "Die", "Mutter", "Das", "Geschehen", "Die", "Granate", "Der", "Renner", "Die", "Klingel"]];
+
+    const newMatch = new Match({
+        shortId: randomShortId,
+        owner: matchOwner,
+        wordList: wordList,
+        playerList: [matchOwner]
+    })
+}
