675012dc7c3053c10a7086bb9b9843b753084a81
hermes <lorenzharfst@gmail.com>
2024-02-29 22:14:39 +0100

    create basic folder structure and match lobby skeleton

---
+++ frontend/src/pages/match.lobby.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// The match Lobby should be where you see the player list and can click PARTIE STARTEN or SPIEL STARTEN (TBD)
+const Lobby = (props) => {
+    // TODO: Create a match, add the ability to copy its link, player list and the button
+}
+
+export default Lobby;
