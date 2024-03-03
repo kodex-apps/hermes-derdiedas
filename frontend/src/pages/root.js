9aee76aebe7ff7fd942cc20c3055e1def0dacedf
hermes <lorenzharfst@gmail.com>
2024-03-03 14:12:01 +0100

    start the root page

---
+++ frontend/src/pages/root.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// The first page users will be in when they load it, it will handle creating a match for the and sending them down a match route
+const Root = (props) => {
+    // TODO: Fill this according to description
+}
+
+export default Root;
