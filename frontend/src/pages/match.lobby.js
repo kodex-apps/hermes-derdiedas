e88659b4a7246a8a25dcdc87c12bd139cc78d6a3
hermes <lorenzharfst@gmail.com>
2024-03-04 22:04:09 +0100

    import Lobby object into Root

--- frontend/src/pages/match.lobby.js
+++ frontend/src/pages/match.lobby.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './match.lobby.css';
 
 // The match Lobby should be where you see the player list and can click PARTIE STARTEN or SPIEL STARTEN (TBD)
 const Lobby = (props) => {
-    // TODO: Create a match, add the ability to copy its link, player list and the button
+    // TODO: Create a match, add the ability to copy its link, player list and the button  
 }
 
 export default Lobby;
--- frontend/src/pages/root.js
+++ frontend/src/pages/root.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Lobby from './match.lobby';
 
 // The first page users will be in when they load it, it will handle creating a match for the and sending them down a match route
 const Root = (props) => {
