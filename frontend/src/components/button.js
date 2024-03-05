407667c11e6a0f30ec138658f246b4dc03c3308b
hermes <lorenzharfst@gmail.com>
2024-03-05 22:00:52 +0100

    add onClick attribute to Button

--- frontend/src/components/button.js
+++ frontend/src/components/button.js
@@ -3,5 +3,5 @@ import './button.css';
 
 const Button = (props) => {
     // Return a button tag with the whatever text is between the tags
-    return <button className="button" role="button">{props.children}</button>
+    return <button onClick={props.onClick} className="button" role="button">{props.children}</button>
 }
