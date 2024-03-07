e071ac81a85f447476212233241afa511cec74fe
hermes <lorenzharfst@gmail.com>
2024-03-07 18:53:05 +0100

    add empty playerlist and playerlist-element

--- frontend/src/pages/lobby.js
+++ frontend/src/pages/lobby.js
@@ -4,7 +4,11 @@ import Button from '../components/button';
 
 // The match Lobby should be where you see the player list and can click PARTIE STARTEN or SPIEL STARTEN (TBD)
 const Lobby = (props) => {
-    return <div className="start-game-button"><Button>SPIEL STARTEN</Button></div>
+    return (
+    <div className="lobby">
+        <div className="playerlist"></div>
+        <div className="start-game-button"><Button>SPIEL STARTEN</Button></div>
+    </div>)
 }
 
 export default Lobby;
