88e9e1c5e85ddd78eaf770add32a739b5117d6f0
hermes <lorenzharfst@gmail.com>
2024-03-07 18:33:00 +0100

    add placeholder image to the root

--- frontend/src/root.js
+++ frontend/src/root.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './root.css';
+import title from './title_placeholder.png';
 
 // The first page users will be in when they load it, it will handle creating a match for the and sending them down a match route
 const Root = props => {
     return (
         <div className="main-container">
+            <img alt="Title" className="title" src={title}/>
             {props.children}
         </div>
         )
