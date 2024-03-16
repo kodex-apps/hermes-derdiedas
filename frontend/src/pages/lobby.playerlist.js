d291d855f40527024d456e28c4261086ee83a190
hermes <lorenzharfst@gmail.com>
2024-03-16 13:39:59 +0100

    sort playerList by placement when initialising PlayerList and paint each player object as PlayerListElement

--- frontend/src/pages/lobby.playerlist.js
+++ frontend/src/pages/lobby.playerlist.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import PlayerListElement from './lobby.playerlist.element';
 
 /* 
 The playerlist shows PlayerListElements for each player and ordered according to the placement of each player.
 */
 const PlayerList = props => {
+    // Sort the playerList by placement
+    props.playerList.sort((a, b) => {
+        if (a.placement > b.placement) return 1;
+        else if (a.placement < b.placement) return -1;
+    })
     //TODO: Add conditional to the table header to show a % column if there is one
     return (
     <table>
         <tr>
             <th>Name</th>
         </tr>
+        {props.playerList.map(player => <PlayerListElement playerObject={player}/>)}
     </table>
     )
 }
