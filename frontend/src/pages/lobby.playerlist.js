87fa592a9714efcfc7d265fc82db84d3d497e1ce
hermes <lorenzharfst@gmail.com>
2024-03-16 10:02:34 +0100

    add the table header for PlayerList

--- frontend/src/pages/lobby.playerlist.js
+++ frontend/src/pages/lobby.playerlist.js
@@ -4,10 +4,12 @@ import React from 'react';
 The playerlist shows PlayerListElements for each player and ordered according to the placement of each player.
 */
 const PlayerList = props => {
-    //TODO: Add the header of the table (Name, % (if there is one))
+    //TODO: Add conditional to the table header to show a % column if there is one
     return (
     <table>
-
+        <tr>
+            <th>Name</th>
+        </tr>
     </table>
     )
 }
