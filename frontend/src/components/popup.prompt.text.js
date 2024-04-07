349bff7f03007665074134d264372676fc7da6f1
hermes <lorenzharfst@gmail.com>
2024-04-07 23:40:43 +0200

    add placeholder input and button to name popup

--- frontend/src/components/popup.prompt.text.js
+++ frontend/src/components/popup.prompt.text.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import Button from './button';
 
 const PopupText = props => {
     /* TODO: Create a popup that asks for a name, the popup's button is a callback function passed through props (use state?) to set the player's name.
     The player's name cannot be repeated (move this text to the callback description)
     */
-	return <Modal>
-	// TODO: Complete the modal
-	</Modal>
+	return <dialog>
+        <form method="dialog">
+            <label>Neuer Name:</label>
+            <input type="text"/>
+            <Button>OK</Button>
+        </form>
+    </dialog>
 }
 
 export default PopupText;
