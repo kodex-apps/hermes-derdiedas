cdb1172006111760efe8f1a66867a9a20fda5da0
hermes <lorenzharfst@gmail.com>
2024-04-08 00:24:02 +0200

    add state variable for dialog open/close

--- frontend/src/components/popup.name.js
+++ frontend/src/components/popup.name.js
@@ -4,12 +4,18 @@ import Button from './button';
 const PopupName = props => {
     /* TODO: Create a popup that asks for a name, the popup's button is a callback function passed through props (use state?) to set the player's name.
     The player's name cannot be repeated (move this text to the callback description)
+    20240408: Added basic dialog. Now create the listener in the edit button of the lobby to create a PopupName object and pass onto it a callback function to set the name (maybe make it get accept an argument with the name?)
     */
+    
+    const [showDialog, setShowDialog] = React.useState(false);
+    const openDialog = () => setShowDialog(true);
+    const closeDialog = () => setShowDialog(false);
+
 	return <dialog>
         <form method="dialog">
-            <label>Neuer Name:</label>
-            <input type="text"/>
-            <Button>OK</Button>
+            <label>Name:</label>
+            <input type="text" autofocus/>
+            <Button onClick={() => {closeDialog(); props.callback();}}>OK</Button>
         </form>
     </dialog>
 }
