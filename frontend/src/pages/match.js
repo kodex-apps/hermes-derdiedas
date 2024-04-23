0e364d22b55a3e352aac37124936a752f2d38591
HERMES <lorenzharfst@gmail.com>
2024-04-23 21:02:00 +0200

    insert TextBox into the Match element

--- frontend/src/pages/match.js
+++ frontend/src/pages/match.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './match.css';
+import TextBox from '../components/match.textbox';
 
 /*
  * A match will be a text box with words showing on its right side. You have to write the articles to the words that appear. As you write them, the cursor stays in the same spot and the text keeps getting pushed to the right, disappearing. Once you write the correct article, it'll light up green and the word will scroll up and fade away, as a new word will fade in and scroll from the bottom into the slot, repeating the process. This means: If you get the article wrong, it'll glow red, and that word will not count towards your end score.
@@ -13,6 +15,7 @@ const loadedWordList = [{article: 'Das', word: 'Haus', isCurrentWord: true},{art
 const Match = props => {
 	[wordList, setWordList] = useState(loadedWordList);
 	return <div className="match">
+		<TextBox />
 	
 	</div>
 }
