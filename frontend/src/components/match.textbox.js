e98d940e84d1e306a38d2d0a411f531b082a1be2
HERMES <lorenzharfst@gmail.com>
2024-04-23 22:50:15 +0200

    make the TextBox background-color --palette-background

--- frontend/src/components/match.textbox.js
+++ frontend/src/components/match.textbox.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './match.textbox.css';
 
 const TextBox = props => {
 	return <input alt="Article Text Box" autoFocus="true" type="text" size="3" className="article-textbox"/>;
