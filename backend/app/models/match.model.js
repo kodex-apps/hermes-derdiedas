9456bbf267ffa6ba3a04bac3472f35066cd9ac43
hermes <lorenzharfst@gmail.com>
2024-05-21 18:13:27 +0200

    overwrite _id in Match Schema

--- backend/app/models/match.model.js
+++ backend/app/models/match.model.js
@@ -2,7 +2,8 @@ module.exports = (mongoose) => {
 	const Match = mongoose.model(
 		"match",
 		mongoose.Schema(
-			{ shortID: Number, wordList: Array, playerList: Array },
+			// Overwriting _id so we can set our own 4 digit IDs
+			{ _id: Number, wordList: Array, playerList: Array },
 			{ timestamps: true },
 		),
 	);
