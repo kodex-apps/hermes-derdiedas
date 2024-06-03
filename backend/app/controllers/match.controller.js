068cef73c45b9da49370835c04a45049fc0c94ed
hermes <lorenzharfst@gmail.com>
2024-06-03 03:18:26 +0200

    define getNewId

--- backend/app/controllers/match.controller.js
+++ backend/app/controllers/match.controller.js
@@ -73,5 +73,3 @@ exports.create = (req, res) => {
 			});
 		});
 };
-
-
--- backend/app/utils/match.getnewid.js
+++ backend/app/utils/match.getnewid.js
@@ -4,11 +4,25 @@ const db = require('../models/index');
 const getNewId = () => {
 	// Array that will hold all the currently used IDs
 	let idArray = [];
-
+	let returnId = 9999;
+	let idFound = false;
 	const MatchModel = db.match;
+
+	// Load into idArray all the _id keys of Matches
 	MatchModel.distinct('_id', {})
 	.then(data => idArray = data)  
 	.catch(err => {console.log(err.message || "Unknown error");});
+	
+	// Get highest id not in the array
+	while (!idFound) {
+		if (idArray.contains(returnId)) returnId--;
+		else idFound
+		
+		// On the case the are no more available IDs, increase the scope
+		if (returnId === 0) returnId = 999999;
+	}
+	
+	return returnId;
 }
 
 module.exports = getNewId;
