a5658eaba249772f5ad7cb1919d1f6f38e538835
hermes <lorenzharfst@gmail.com>
2024-06-28 18:30:05 +0200

    change playerName from body to URL in the API call

--- frontend/src/utils/getnewmatch.js
+++ frontend/src/utils/getnewmatch.js
@@ -3,15 +3,12 @@ const config = require("./config");
 export default async function getMatch(playerName) {
 	//TODO: Define the config file and import here as config
 	console.log(config.fetchUrl);
-	const res = await fetch(config.fetchUrl, {
+	const res = await fetch(config.fetchUrl + `/${playerName}`, {
 		method: "POST",
 		headers: {
 			"content-type": "application/json",
 		},
 		mode: "no-cors",
-		body: {
-			"ownerName": `${playerName}`
-		}
 	});
 
 	return res;
