0c41c7ee3d65ab7f1e1c8753ec05893357b6d803
hermes <lorenzharfst@gmail.com>
2024-06-30 16:00:10 +0200

    remove show url in console when api call (no longer needed)

--- frontend/src/utils/getnewmatch.js
+++ frontend/src/utils/getnewmatch.js
@@ -2,7 +2,6 @@ const config = require("./config");
 
 export default async function getMatch(playerName) {
 	//TODO: Define the config file and import here as config
-	console.log(config.fetchUrl);
 	const res = await fetch(config.fetchUrl + `/${playerName}`, {
 		method: "POST",
 		headers: {
