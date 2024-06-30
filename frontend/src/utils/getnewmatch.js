0c7ba94f4ade4eb5c937ef7cb672a351b9a342ae
hermes <lorenzharfst@gmail.com>
2024-06-30 16:03:21 +0200

    add create to api call param

--- frontend/src/utils/getnewmatch.js
+++ frontend/src/utils/getnewmatch.js
@@ -2,7 +2,7 @@ const config = require("./config");
 
 export default async function getMatch(playerName) {
 	//TODO: Define the config file and import here as config
-	const res = await fetch(config.fetchUrl + `/${playerName}`, {
+	const res = await fetch(config.fetchUrl + `/create/${playerName}`, {
 		method: "POST",
 		headers: {
 			"content-type": "application/json",
