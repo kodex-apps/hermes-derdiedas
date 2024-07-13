babd4905e74fed61f685433d70e9fd34205acaec
hermes <lorenzharfst@gmail.com>
2024-07-13 18:00:00 +0200

    making getNewId be a promise in itself

--- backend/app/utils/match.getnewid.js
+++ backend/app/utils/match.getnewid.js
@@ -1,7 +1,8 @@
 const db = require('../models/index');
 
 // Returns an available ID
-async function getNewId() {
+/* Commented out for debugging purposes
+ * async function getNewId() {
 	// Array that will hold all the currently used IDs
 	let idArray = [];
 	let idFound = false;
@@ -30,6 +31,39 @@ async function getNewId() {
 	});
 
 	return promiseId;
+}
+*/
+
+const getNewId = () => new Promise((res, err) => {
+	// Array that will hold all the currently used IDs
+	let idArray = [];
+	let idFound = false;
+	const MatchModel = db.match;
+
+	let promiseId = await new Promise(res => {
+		// Load into idArray all the _id keys of Matches
+		const returnId = MatchModel.distinct('_id', {})
+		.then(data => {
+			idArray = data; 
+			console.log("Checking availability on this idArray:" + idArray);
+			// Get highest id not in the array
+			while (!idFound) {
+				if (idArray.includes(returnId)) {returnId--; console.log("ID already exists, now trying new ID: " + returnId);}
+				else {idFound = true;console.log("Creating match, with id: " + returnId);}
+				// On the case the are no more available IDs, increase the scope
+				if (returnId === 0) returnId = 999999;
+			}
+			console.log("Exiting while, creating match with id " + returnId);
+
+
+		})  
+		.catch(err => console.log(err.message || "Unknown error"));
+
+		res(returnId);
+	});
+
+	res(promiseId);
+	
 }
 
 module.exports = getNewId;
