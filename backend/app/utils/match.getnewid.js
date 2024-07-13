62f8b4901e00f2630c05c6784667cf0d7c7db9b0
hermes <lorenzharfst@gmail.com>
2024-07-13 18:12:29 +0200

    remove internal promise and await db call

--- backend/app/utils/match.getnewid.js
+++ backend/app/utils/match.getnewid.js
@@ -6,29 +6,28 @@ async function getNewId() {
 	let idArray = [];
 	let idFound = false;
 	const MatchModel = db.match;
+	let returnId = 9999;
 
-	let promiseId = await new Promise(res => {
-		// Load into idArray all the _id keys of Matches
-		const returnId = MatchModel.distinct('_id', {})
-		.then(data => {
-			idArray = data; 
-			console.log("Checking availability on this idArray:" + idArray);
-			// Get highest id not in the array
-			while (!idFound) {
-				if (idArray.includes(returnId)) {returnId--; console.log("ID already exists, now trying new ID: " + returnId);}
-				else {idFound = true;console.log("Creating match, with id: " + returnId);}
-				// On the case the are no more available IDs, increase the scope
-				if (returnId === 0) returnId = 999999;
-			}
-			console.log("Exiting while, creating match with id " + returnId);
+	// Load into idArray all the _id keys of Matches
+	await MatchModel.distinct('_id', {})
+	.then(data => {
+		idArray = data; 
+		console.log("Checking availability on this idArray:" + idArray);
+		// Get highest id not in the array
+		while (!idFound) {
+			if (idArray.includes(returnId)) {returnId--; console.log("ID already exists, now trying new ID: " + returnId);}
+			else {idFound = true;console.log("Creating match, with id: " + returnId);}
+			// On the case the are no more available IDs, increase the scope
+			if (returnId === 0) returnId = 999999;
+		}
+		console.log("Exiting while, creating match with id " + returnId);
 
-			res(returnId);
+		res(returnId);
 
+	})  
+	.catch(err => console.log(err.message || "Unknown error"));
 
-		})  
-		.catch(err => console.log(err.message || "Unknown error"));
-
-	});
+	
 
 	return promiseId;
 }
