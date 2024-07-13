e9b1dd19027ad25e7894eb6f9fae80335a7a6680
hermes <lorenzharfst@gmail.com>
2024-07-13 12:34:02 +0200

    make promiseId a promise and return that

--- backend/app/utils/match.getnewid.js
+++ backend/app/utils/match.getnewid.js
@@ -4,28 +4,31 @@ const db = require('../models/index');
 async function getNewId() {
 	// Array that will hold all the currently used IDs
 	let idArray = [];
-	let returnId = 9999;
 	let idFound = false;
 	const MatchModel = db.match;
 
-	// Load into idArray all the _id keys of Matches
-	returnId = await MatchModel.distinct('_id', {})
-	.then(data => {
-		idArray = data; 
-		console.log("Checking availability on this idArray:" + idArray);
-		// Get highest id not in the array
-		while (!idFound) {
-			if (idArray.includes(returnId)) {returnId--; console.log("ID already exists, now trying new ID: " + returnId);}
-			else {idFound = true;console.log("Creating match, with id: " + returnId);}
-			// On the case the are no more available IDs, increase the scope
-			if (returnId === 0) returnId = 999999;
-		}
-		console.log("Exiting while, creating match with id " + returnId);
+	let promiseId = new Promise(res => {
+		// Load into idArray all the _id keys of Matches
+		returnId = await MatchModel.distinct('_id', {})
+		.then(data => {
+			idArray = data; 
+			console.log("Checking availability on this idArray:" + idArray);
+			// Get highest id not in the array
+			while (!idFound) {
+				if (idArray.includes(returnId)) {returnId--; console.log("ID already exists, now trying new ID: " + returnId);}
+				else {idFound = true;console.log("Creating match, with id: " + returnId);}
+				// On the case the are no more available IDs, increase the scope
+				if (returnId === 0) returnId = 999999;
+			}
+			console.log("Exiting while, creating match with id " + returnId);
 
-	})  
-	.catch(err => {console.log(err.message || "Unknown error");});
-	
-	return returnId;
+
+		})  
+		.catch(err => {console.log(err.message || "Unknown error");});
+
+		res(returnId);
+	}
+	return promiseId;
 }
 
 module.exports = getNewId;
