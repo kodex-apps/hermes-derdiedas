c7c9aad8a38d0e9cdec357e75ad67dd84fb513e3
hermes <lorenzharfst@gmail.com>
2024-07-24 22:33:32 +0200

    since it responds with an array, set send data to [0]

--- backend/app/controllers/match.controller.js
+++ backend/app/controllers/match.controller.js
@@ -9,7 +9,7 @@ exports.findOne = (req, res) => {
 	Match.find({ _id: matchId })
 		.then((data) => {
 			if (!data) res.status(404).send({ message: `Couldn't find match ${id}` });
-			else res.send(data);
+			else res.send(data[0]);
 		})
 		.catch((err) => {
 			res
@@ -19,12 +19,7 @@ exports.findOne = (req, res) => {
 };
 
 // Function to create a new Lobby, it will return a new matchId. It will create a Match document with the given playerList and the created matchId
-exports.create = (req, res) => {
-	const matchOwner = req.params.ownerName;
-	// Temp shortId for testing purposes. TODO: Create the actual generation of a short and unique id for each match, made of numbers
-	// 1. If there isn't a matchId, check db for the oldest available Id and apply it to this match. This could be maybe done by getting a list of all matchIds, then starting with 001 it checks the array until one matchId is available and set that to the Match being created. To do this, sort the array of matchIds, and compare each element with its index, as soon as you find one where the index+1 isn't equivalent to the matchId (because we don't start with 0 in the matchIds) it means the number below the current matchId is free to take.
-	// 2. Get 10 random words + articles and send them as a single array (let the client do the processing into an object, although maybe it'll be easier to maintain the model that already puts the article + word into a single object
-	// 3. Put them into a Match object and respond with it
+exports.create = (req, res) => { const matchOwner = req.params.ownerName; // Temp shortId for testing purposes. TODO: Create the actual generation of a short and unique id for each match, made of numbers 1. If there isn't a matchId, check db for the oldest available Id and apply it to this match. This could be maybe done by getting a list of all matchIds, then starting with 001 it checks the array until one matchId is available and set that to the Match being created. To do this, sort the array of matchIds, and compare each element with its index, as soon as you find one where the index+1 isn't equivalent to the matchId (because we don't start with 0 in the matchIds) it means the number below the current matchId is free to take. 2. Get 10 random words + articles and send them as a single array (let the client do the processing into an object, although maybe it'll be easier to maintain the model that already puts the article + word into a single object 3. Put them into a Match object and respond with it
 	
 	// Temp wordList for testing purposes.
 	const loadedWords = [
