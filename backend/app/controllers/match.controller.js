5ae4c685ff19046b621f0b909b137cb52ff3c702
hermes <lorenzharfst@gmail.com>
2024-08-28 15:54:21 +0200

    check if all players wordCount is 10 and if match isOngoing = true, set it to false

--- backend/app/controllers/match.controller.js
+++ backend/app/controllers/match.controller.js
@@ -63,6 +63,10 @@ exports.update = (req, res) => {
 			// If the player already exists, substitute it with the new player object. If it's new, assign it an id and add it to the playerList.
 			if (indexOfPlayer != -1) {
 				latestMatch.playerList[indexOfPlayer] = player;
+				// Check if all players have wordsCompleted = 10 and the match is still onGoing to finish it
+				if ((latestMatch.playerList.findIndex(e => e.wordsCompleted != 10) = -1) && latestMatch.isOngoing) {
+					latestMatch.isOngoig = false;
+				}
 				console.log("Player was found in match, substituting.");
 			}
 			else {
