13ff83e328747a0f7362fc7faecb5f617c70e43c
HERMES <lorenzharfst@gmail.com>
2024-09-08 20:40:12 +0000

    create util function to provide startMatch with a generated wordList

--- backend/app/utils/match.getwordlist.js
+++ backend/app/utils/match.getwordlist.js
@@ -1,26 +1,31 @@
 const fs = require('node:fs');
 
 function getWordList() {
+	let rawWordList;
 	let wordList = [];
 
-	// Get the full word list and assign 10 random of them to our wordList
-	fs.readFile('./wordlist', 'utf8', (err, data) => {
-	if (err) { console.error(err); return; }
-	let fullWordList = data.split("\n").sort(() => 0.5 - Math.random());
-
-	// Getting 10 strings of words from the random fullWordList
-	rawWordList = fullWordList.slice(0, num);
-	console.log("Raw World List: ");
-	console.log(rawWordList);
+	try {
+		const data = fs.readFileSync('app/utils/wortschatz', 'utf8');
+		// Get the full word list and assign 10 random of them to our wordList
+		let fullWordList = data.split("\n").sort(() => 0.5 - Math.random());
+		// Getting 10 strings of words from the random fullWordList
+		rawWordList = fullWordList.slice(0, 10);
+		console.log("Raw World List: ");
+		console.log(rawWordList);
+	} catch (err) {
+		console.error(err); return;
 	}
 
 	// Add aditional data for the app to each word
 	rawWordList.forEach(e => {
 		currentWord = e.split(' ');
-		wordList.push({	article: word[0], word: word[1], isCurrentWord: false, isCorrectWord: null});
-	}
+		wordList.push({	article: currentWord[0], word: currentWord[1], isCurrentWord: false, isCorrectWord: null});
+	});
+	// Set the first word to be the current word (match.js expects one to have it)
 	wordList[0].isCurrentWord = true;
 
 	console.log("Final Word List: ");
 	console.log(wordList);
 }
+
+module.exports = getWordList;
